34c2d7e4faaf4e1b735779ccb8e5c26ff988bf49
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-11 14:02:08 -0500

    chore: added schedule entity model.

---
+++ TecFinance-Backend.API/Payments/Domain/Models/Schedule.cs
@@ -0,0 +1,15 @@
+﻿namespace TecFinance_Backend.API.Payments.Domain.Models;
+
+public class Schedule
+{
+    public Schedule(List<Payment> payments)
+    {
+        Payments = payments;
+    }
+
+    public int Id { get; set; }
+    
+    
+    // Relationships
+    public List<Payment> Payments { get; set; }
+}
