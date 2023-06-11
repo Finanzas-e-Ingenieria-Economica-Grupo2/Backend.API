5b60ece90c1d8be5bd589bb4a0fda6c40441438c
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-11 18:17:56 -0500

    feat: added domain for share bounded context.

---
+++ TecFinance-Backend.API/Shared/Domain/Services/Communication/BaseResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace TecFinance_Backend.API.Shared.Domain.Services.Communication;
+
+public abstract class BaseResponse<T>
+{
+    protected BaseResponse(string message)
+    {
+        Success = false;
+        Message = message;
+        Resource = default;
+    }
+
+    protected BaseResponse(T resource)
+    {
+        Success = true;
+        Message = string.Empty;
+        Resource = resource;
+    }
+
+    public bool Success { get; protected set; }
+    
+    public string Message { get; protected set; }
+    
+    public T Resource { get; protected set; }
+}
