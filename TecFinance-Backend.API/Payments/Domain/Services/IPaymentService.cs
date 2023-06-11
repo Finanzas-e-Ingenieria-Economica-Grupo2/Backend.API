2eb3e7ef2230a784226125fed0794493ed12cd64
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-11 18:57:26 -0500

    feat: added contract services for payment and schedule.

---
+++ TecFinance-Backend.API/Payments/Domain/Services/IPaymentService.cs
@@ -0,0 +1,13 @@
+﻿using TecFinance_Backend.API.Payments.Domain.Models;
+using TecFinance_Backend.API.Payments.Domain.Services.Communication;
+
+namespace TecFinance_Backend.API.Payments.Domain.Services;
+
+public interface IPaymentService
+{
+    Task<IEnumerable<Payment>> ListAsync();
+    Task<IEnumerable<Payment>> ListByScheduleIdAsync();
+    Task<PaymentResponse> SaveAsync(Payment payment);
+    Task<PaymentResponse> UpdateAsync(int id, Payment payment);
+    Task<PaymentResponse> DeleteAsync(int id);
+}
