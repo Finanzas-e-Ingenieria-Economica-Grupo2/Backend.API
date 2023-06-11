9ccc290bbf75ad476f5e0d359891f4611baaff78
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-11 14:01:47 -0500

    chore: added payment entity model.

---
+++ TecFinance-Backend.API/Payments/Domain/Models/Payment.cs
@@ -0,0 +1,29 @@
+﻿namespace TecFinance_Backend.API.Payments.Domain.Models;
+
+public class Payment
+{
+    // Missing currency
+
+    public int Id { get; set; }
+    public decimal Tep { get; set; }
+    public string GracePeriod { get; set; }
+    public decimal InitialBalance { get; set; }
+    public decimal FinalBalance { get; set; }
+    public decimal Interest { get; set; }
+    public decimal Amortization { get; set; }
+    public decimal Fee { get; set; }
+    public decimal TotalFee { get; set; }
+
+    // Seguro de Desgravamen
+    public decimal LienInsurance { get; set; } 
+    // Seguro de Inmueble
+    public decimal PropertyInsurance { get; set; }
+    // Gasto de tasación
+    public decimal ValuationExpenses { get; set; }
+
+    
+    // Relationships
+    public int ScheduleId { get; set; }
+    public Schedule Schedule { get; set; }
+
+}
