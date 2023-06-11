3a642d601d776e42fe9e6e832e15dc737e2cc387
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-11 18:31:10 -0500

    feat: added contracts repositories for payment and schedule.

---
+++ TecFinance-Backend.API/Payments/Domain/Repositories/IPaymentRepository.cs
@@ -0,0 +1,13 @@
+﻿using TecFinance_Backend.API.Payments.Domain.Models;
+
+namespace TecFinance_Backend.API.Payments.Domain.Repositories;
+
+public interface IPaymentRepository
+{
+    Task<IEnumerable<Payment>> ListAsync();
+    Task<IEnumerable<Payment>> ListByScheduleIdAsync();
+    Task<Payment> FindByIdAsync(int id);
+    Task AddAsync(Payment payment);
+    void Update(Payment category);
+    void Remove(Payment category);
+}
---
+++ TecFinance-Backend.API/Payments/Domain/Repositories/IScheduleRepository.cs
@@ -0,0 +1,12 @@
+﻿using TecFinance_Backend.API.Payments.Domain.Models;
+
+namespace TecFinance_Backend.API.Payments.Domain.Repositories;
+
+public interface IScheduleRepository
+{
+    Task<IEnumerable<Schedule>> ListAsync();
+    Task<Schedule> FindByIdAsync(int id);
+    Task AddAsync(Schedule payment);
+    void Update(Schedule category);
+    void Remove(Schedule category);
+}
