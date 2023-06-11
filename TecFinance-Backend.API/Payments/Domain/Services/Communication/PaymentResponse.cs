a2149b2f9c2c332ffd8591b20e1cb4c640fbe3eb
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-11 18:39:31 -0500

    feat: added implementation of communication service for payment and schedule.

---
+++ TecFinance-Backend.API/Payments/Domain/Services/Communication/PaymentResponse.cs
@@ -0,0 +1,15 @@
+﻿using TecFinance_Backend.API.Payments.Domain.Models;
+using TecFinance_Backend.API.Shared.Domain.Services.Communication;
+
+namespace TecFinance_Backend.API.Payments.Domain.Services.Communication;
+
+public class PaymentResponse: BaseResponse<Payment>
+{
+    public PaymentResponse(string message) : base(message)
+    {
+    }
+
+    public PaymentResponse(Payment resource) : base(resource)
+    {
+    }
+}
