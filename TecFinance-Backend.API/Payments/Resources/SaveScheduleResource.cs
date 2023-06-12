294834206876b9bd5f53a58eaa6b3e87678850c8
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-11 19:33:53 -0500

    feat: added resources for payment and schedule.

---
+++ TecFinance-Backend.API/Payments/Resources/SaveScheduleResource.cs
@@ -0,0 +1,6 @@
+﻿namespace TecFinance_Backend.API.Payments.Resources;
+
+public class SaveScheduleResource
+{
+    // Empty for the moment
+}
---
+++ TecFinance-Backend.API/Payments/Resources/ScheduleResource.cs
@@ -0,0 +1,6 @@
+﻿namespace TecFinance_Backend.API.Payments.Resources;
+
+public class ScheduleResource
+{
+    public int Id { get; set; }
+}
