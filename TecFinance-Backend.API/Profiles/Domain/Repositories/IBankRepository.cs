1414a299110758f97c7181fa08ffd6d0f4e4990c
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-15 16:23:43 -0500

    feat: added repository contract for bank.

---
+++ TecFinance-Backend.API/Profiles/Domain/Repositories/IBankRepository.cs
@@ -0,0 +1,12 @@
+﻿using TecFinance_Backend.API.Profiles.Domain.Models;
+
+namespace TecFinance_Backend.API.Profiles.Domain.Repositories;
+
+public interface IBankRepository
+{
+    Task<IEnumerable<Bank>> ListAsync();
+    Task<Bank> FindByIdAsync(int id);
+    Task AddAsync(Bank bank);
+    void Update(Bank bank);
+    void Remove(Bank bank);
+}
