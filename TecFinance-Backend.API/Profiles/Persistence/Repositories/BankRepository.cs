f0e6b8e244f4d395e72e2d16c5054f0f04e06cd3
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 17:18:44 -0500

    feat: added repository implementation for user and bank.

---
+++ TecFinance-Backend.API/Profiles/Persistence/Repositories/BankRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TecFinance_Backend.API.Profiles.Domain.Models;
+using TecFinance_Backend.API.Profiles.Domain.Repositories;
+using TecFinance_Backend.API.Shared.Persistence.Contexts;
+using TecFinance_Backend.API.Shared.Persistence.Repositories;
+
+namespace TecFinance_Backend.API.Profiles.Persistence.Repositories;
+
+public class BankRepository : BaseRepository, IBankRepository
+{
+    public BankRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<Bank>> ListAsync()
+    {
+        return await _context.Banks.ToListAsync();
+    }
+
+    public async Task<Bank> FindByIdAsync(int bankId)
+    {
+        return await _context.Banks.FindAsync(bankId);
+    }
+
+    public async Task AddAsync(Bank bank)
+    {
+        await _context.Banks.AddAsync(bank);
+    }
+
+    public void Update(Bank bank)
+    {
+        _context.Banks.Update(bank);
+    }
+
+    public void Remove(Bank bank)
+    {
+        _context.Banks.Remove(bank);
+    }
+}
---
+++ TecFinance-Backend.API/Profiles/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TecFinance_Backend.API.Profiles.Domain.Models;
+using TecFinance_Backend.API.Profiles.Domain.Repositories;
+using TecFinance_Backend.API.Shared.Persistence.Contexts;
+using TecFinance_Backend.API.Shared.Persistence.Repositories;
+
+namespace TecFinance_Backend.API.Profiles.Persistence.Repositories;
+
+public class UserRepository : BaseRepository, IUserRepository
+{
+    public UserRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<User>> ListAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task<User> FindByIdAsync(int userId)
+    {
+        return await _context.Users.FindAsync(userId);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public void Update(User user)
+    {
+        _context.Users.Update(user);
+    }
+
+    public void Remove(User user)
+    {
+        _context.Users.Remove(user);
+    }
+}
