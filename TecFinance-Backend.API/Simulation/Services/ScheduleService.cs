61d7ee71948f8a7b7cc76c28cca95129cfa8957c
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 11:26:13 -0500

    feat: added services implementation for schedule.

--- TecFinance-Backend.API/Simulation/Services/ScheduleService.cs
+++ TecFinance-Backend.API/Simulation/Services/ScheduleService.cs
@@ -1,4 +1,6 @@
-﻿using TecFinance_Backend.API.Simulation.Domain.Models;
+﻿using TecFinance_Backend.API.Shared.Domain.Repositories;
+using TecFinance_Backend.API.Simulation.Domain.Models;
+using TecFinance_Backend.API.Simulation.Domain.Repositories;
 using TecFinance_Backend.API.Simulation.Domain.Services;
 using TecFinance_Backend.API.Simulation.Domain.Services.Communication;
 
@@ -6,23 +8,92 @@ namespace TecFinance_Backend.API.Simulation.Services;
 
 public class ScheduleService : IScheduleService
 {
-    public Task<IEnumerable<Schedule>> ListAsync()
+    private readonly IScheduleRepository _scheduleRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ScheduleService(IScheduleRepository scheduleRepository, IUnitOfWork unitOfWork)
     {
-        throw new NotImplementedException();
+        _scheduleRepository = scheduleRepository;
+        _unitOfWork = unitOfWork;
     }
 
-    public Task<ScheduleResponse> SaveAsync(Schedule schedule)
+    public async Task<IEnumerable<Schedule>> ListAsync()
     {
-        throw new NotImplementedException();
+        return await _scheduleRepository.ListAsync();
     }
 
-    public Task<ScheduleResponse> UpdateAsync(int id, Schedule schedule)
+    public async Task<ScheduleResponse> SaveAsync(Schedule schedule)
     {
-        throw new NotImplementedException();
+        // Validate existence of assigned schedule
+        
+
+        // Perform adding
+        
+        try
+        {
+            await _scheduleRepository.AddAsync(schedule);
+            await _unitOfWork.CompleteAsync();
+            
+            return new ScheduleResponse(schedule);
+        }
+        catch (Exception e)
+        {
+            // Do some logging stuff
+            return new ScheduleResponse($"An error occurred while saving the schedule: {e.Message}");
+        }    
     }
 
-    public Task<ScheduleResponse> DeleteAsync(int id)
+    public async Task<ScheduleResponse> UpdateAsync(int id, Schedule schedule)
     {
-        throw new NotImplementedException();
+        // Validate if schedule exists
+
+        var existingSchedule = await _scheduleRepository.FindByIdAsync(id);
+
+        if (existingSchedule == null)
+            return new ScheduleResponse("Schedule not found.");
+
+        // Modify fields
+        
+        existingSchedule.Payments = schedule.Payments;
+        
+        // Perform update
+        
+        try
+        {
+            _scheduleRepository.Update(existingSchedule);
+            await _unitOfWork.CompleteAsync();
+            
+            return new ScheduleResponse(existingSchedule);
+        }
+        catch (Exception e)
+        {
+            // Error handling
+            return new ScheduleResponse($"An error occurred while updating the schedule: {e.Message}");
+        }
+    }
+
+    public async Task<ScheduleResponse> DeleteAsync(int id)
+    {
+        // Validate if schedule exists
+        
+        var existingSchedule = await _scheduleRepository.FindByIdAsync(id);
+        
+        if (existingSchedule == null)
+            return new ScheduleResponse("Schedule not found.");
+        
+        // Perform delete
+        
+        try
+        {
+            _scheduleRepository.Remove(existingSchedule);
+            await _unitOfWork.CompleteAsync();
+            
+            return new ScheduleResponse(existingSchedule);
+        }
+        catch (Exception e)
+        {
+            // Error handling
+            return new ScheduleResponse($"An error occurred while deleting the payment: {e.Message}");
+        }
     }
 }
