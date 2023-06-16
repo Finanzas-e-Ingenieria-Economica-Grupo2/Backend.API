656083604bdb9bd68f6d4e43cd5e570ce4c0cc4f
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 15:49:26 -0500

    feat: added user services contracts.

---
+++ TecFinance-Backend.API/Profiles/Domain/Services/Communication/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using TecFinance_Backend.API.Profiles.Domain.Models;
+using TecFinance_Backend.API.Shared.Domain.Services.Communication;
+
+namespace TecFinance_Backend.API.Profiles.Domain.Services.Communication;
+
+public class UserResponse : BaseResponse<User>
+{
+    public UserResponse(string message) : base(message)
+    {
+    }
+
+    public UserResponse(User resource) : base(resource)
+    {
+    }
+}
---
+++ TecFinance-Backend.API/Profiles/Domain/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using TecFinance_Backend.API.Profiles.Domain.Models;
+using TecFinance_Backend.API.Profiles.Domain.Services.Communication;
+
+namespace TecFinance_Backend.API.Profiles.Domain.Services;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task<UserResponse> SaveAsync(User user);
+    Task<UserResponse> UpdateAsync(int id, User user);
+    Task<UserResponse> DeleteAsync(int id);
+}
