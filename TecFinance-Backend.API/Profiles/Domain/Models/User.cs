5087d0b6f198eca8b83d9d73568c716d25a7de02
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 15:30:05 -0500

    feat: added user entity.

---
+++ TecFinance-Backend.API/Profiles/Domain/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace TecFinance_Backend.API.Profiles.Domain.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Mail { get; set; }
+    public string Password { get; set; }
+}
