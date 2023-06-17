fd2443821c3eb3b5b3f501c11ff44a0e1592994b
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 19:20:17 -0500

    chore: added attributes for offer's resources.

--- TecFinance-Backend.API/Simulation/Resources/OfferResource.cs
+++ TecFinance-Backend.API/Simulation/Resources/OfferResource.cs
@@ -3,4 +3,15 @@
 public class OfferResource
 {
     public int Id { get; set; }
+    public decimal HomeValue { get; set; }
+    public decimal AmountToFinance { get; set; }
+    public bool IsHousingSupport { get; set; }
+    public bool IsHousingSustainable { get; set; }
+    public decimal Tea { get; set; }
+    public decimal Tna { get; set; }
+    public string Capitalization { get; set; }
+    public int TermInMonths { get; set; }
+    public decimal Tcea { get; set; }
+    public decimal Van { get; set; }
+    public decimal Tir { get; set; }
 }
--- TecFinance-Backend.API/Simulation/Resources/SaveOfferResource.cs
+++ TecFinance-Backend.API/Simulation/Resources/SaveOfferResource.cs
@@ -1,6 +1,46 @@
-﻿namespace TecFinance_Backend.API.Simulation.Resources;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecFinance_Backend.API.Simulation.Resources;
 
 public class SaveOfferResource
 {
-    // Empty for the moment
+    [Required]
+    public decimal HomeValue { get; set; }
+    
+    [Required]
+    public decimal AmountToFinance { get; set; }
+    
+    [Required]
+    public bool IsHousingSupport { get; set; }
+    
+    [Required]
+    public bool IsHousingSustainable { get; set; }
+    
+    [Required]
+    public decimal Tea { get; set; }
+    
+    [Required]
+    public decimal Tna { get; set; }
+    
+    [Required]
+    [MaxLength(20)]
+    public string Capitalization { get; set; }
+    
+    [Required]
+    public int TermInMonths { get; set; }
+    
+    [Required]
+    public decimal Tcea { get; set; }
+    
+    [Required]
+    public decimal Van { get; set; }
+    
+    [Required]
+    public decimal Tir { get; set; }
+    
+    [Required]
+    public int UserId { get; set; }
+    
+    [Required]
+    public int BankId { get; set; }
 }
