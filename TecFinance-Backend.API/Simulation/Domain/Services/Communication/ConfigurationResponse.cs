ba9084989fce96bb189f2f983e746fb2e1cc7d02
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 19:57:24 -0500

    feat: added services contract for configuration.

---
+++ TecFinance-Backend.API/Simulation/Domain/Services/Communication/ConfigurationResponse.cs
@@ -0,0 +1,15 @@
+﻿using TecFinance_Backend.API.Shared.Domain.Services.Communication;
+using TecFinance_Backend.API.Simulation.Domain.Models;
+
+namespace TecFinance_Backend.API.Simulation.Domain.Services.Communication;
+
+public class ConfigurationResponse : BaseResponse<Configuration>
+{
+    public ConfigurationResponse(string message) : base(message)
+    {
+    }
+
+    public ConfigurationResponse(Configuration resource) : base(resource)
+    {
+    }
+}
---
+++ TecFinance-Backend.API/Simulation/Domain/Services/IConfigurationService.cs
@@ -0,0 +1,12 @@
+﻿using TecFinance_Backend.API.Simulation.Domain.Models;
+using TecFinance_Backend.API.Simulation.Domain.Services.Communication;
+
+namespace TecFinance_Backend.API.Simulation.Domain.Services;
+
+public interface IConfigurationService
+{
+    Task<IEnumerable<Configuration>> ListAsync();
+    Task<ConfigurationResponse> SaveAsync(Configuration tutorial);
+    Task<ConfigurationResponse> UpdateAsync(int tutorialId, Configuration tutorial);
+    Task<ConfigurationResponse> DeleteAsync(int tutorialId);
+}
