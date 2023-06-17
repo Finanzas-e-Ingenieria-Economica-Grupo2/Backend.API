abf2b4ed584583d0e368e65c3771eae79d584863
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 20:03:49 -0500

    feat: added resources for configuration.

---
+++ TecFinance-Backend.API/Simulation/Resources/ConfigurationResource.cs
@@ -0,0 +1,10 @@
+﻿namespace TecFinance_Backend.API.Simulation.Resources;
+
+public class ConfigurationResource
+{
+    public int Id { get; set; }
+    public string InterestRateType { get; set; }
+    public string Currency { get; set; }
+    public int AmountTotalGracePeriod { get; set; }
+    public int AmountPartialGracePeriod { get; set; }
+}
---
+++ TecFinance-Backend.API/Simulation/Resources/SaveConfigurationResource.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TecFinance_Backend.API.Simulation.Resources;
+
+public class SaveConfigurationResource
+{
+    [Required]
+    [MaxLength(10)]
+    public string InterestRateType { get; set; }
+    
+    [Required]
+    [MaxLength(10)]
+    public string Currency { get; set; }
+    
+    [Required]
+    public int AmountTotalGracePeriod { get; set; }
+    
+    [Required]
+    public int AmountPartialGracePeriod { get; set; }
+    
+    [Required]
+    public int OfferId { get; set; }
+}
