568ce52a1bf16c8d792ffb569d63ed21ce4f2c86
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 19:53:52 -0500

    feat: added repository contract for configuration.

---
+++ TecFinance-Backend.API/Simulation/Domain/Repositories/IConfigurationRepository.cs
@@ -0,0 +1,13 @@
+﻿using TecFinance_Backend.API.Simulation.Domain.Models;
+
+namespace TecFinance_Backend.API.Simulation.Domain.Repositories;
+
+public interface IConfigurationRepository
+{
+    Task<IEnumerable<Configuration>> ListAsync();
+    Task<Configuration> FindByIdAsync(int tutorialId);
+    Task<IEnumerable<Configuration>> FindByOfferIdAsync(int offerId);
+    Task AddAsync(Configuration configuration);
+    void Update(Configuration configuration);
+    void Remove(Configuration configuration);
+}
