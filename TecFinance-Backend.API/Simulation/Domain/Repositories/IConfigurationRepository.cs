45a14ff03e3524b872682303f05e9f635eee8957
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2023-06-16 21:44:12 -0500

    feat: added repository implementation for configuration entity.

--- TecFinance-Backend.API/Simulation/Domain/Repositories/IConfigurationRepository.cs
+++ TecFinance-Backend.API/Simulation/Domain/Repositories/IConfigurationRepository.cs
@@ -5,8 +5,8 @@ namespace TecFinance_Backend.API.Simulation.Domain.Repositories;
 public interface IConfigurationRepository
 {
     Task<IEnumerable<Configuration>> ListAsync();
-    Task<Configuration> FindByIdAsync(int tutorialId);
-    Task<IEnumerable<Configuration>> FindByOfferIdAsync(int offerId);
+    Task<Configuration> FindByIdAsync(int configurationId);
+    Task<Configuration> FindByOfferIdAsync(int offerId);
     Task AddAsync(Configuration configuration);
     void Update(Configuration configuration);
     void Remove(Configuration configuration);
